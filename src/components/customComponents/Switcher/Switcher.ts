46edf13b24c27932353d4e7338a97e14a2b3d1c7
mike connor <mike@mikeconnor.tech>
2024-02-01 13:30:17 -0600

    fix properties

--- src/components/customComponents/Switcher/Switcher.ts
+++ src/components/customComponents/Switcher/Switcher.ts
@@ -6,15 +6,17 @@
  * @property {integer} limit=4 A number representing the maximum number of items permitted for a horizontal layout
  */
 export default class Switcher extends HTMLElement {
-  constructor() {
-    super();
-    this.render = () => {
-      this.i = `Switcher-${[this.threshold, this.space, this.limit].join('')}`;
-      this.dataset.i = this.i;
-      if (!document.getElementById(this.i)) {
-        let styleEl = document.createElement('style');
-        styleEl.id = this.i;
-        styleEl.innerHTML = `
+    render: () => void
+    i!: string
+    constructor() {
+        super()
+        this.render = () => {
+            this.i = `Switcher-${[this.threshold, this.space, this.limit].join('')}`
+            this.dataset.i = this.i
+            if (!document.getElementById(this.i)) {
+                let styleEl = document.createElement('style')
+                styleEl.id = this.i
+                styleEl.innerHTML = `
           [data-i="${this.i}"] {
             gap: ${this.space};
           }
@@ -27,49 +29,50 @@ export default class Switcher extends HTMLElement {
           [data-i="${this.i}"] > :nth-last-child(n+${parseInt(this.limit) + 1}) ~ * {
             flex-basis: 100%;
           }
-        `.replace(/\s\s+/g, ' ').trim();
-        document.head.appendChild(styleEl);
-      }
+        `
+                    .replace(/\s\s+/g, ' ')
+                    .trim()
+                document.head.appendChild(styleEl)
+            }
+        }
     }
-  }
-  get threshold() {
-    return this.getAttribute('threshold') || 'var(--measure)';
-  }
+    get threshold() {
+        return this.getAttribute('threshold') || 'var(--measure)'
+    }
 
-  set threshold(val) {
-    return this.setAttribute('threshold', val);
-  }
+    set threshold(val) {
+        return this.setAttribute('threshold', val)
+    }
 
-  get space() {
-    return this.getAttribute('space') || 'var(--s1)';
-  }
+    get space() {
+        return this.getAttribute('space') || 'var(--s1)'
+    }
 
-  set space(val) {
-    return this.setAttribute('space', val);
-  }
+    set space(val) {
+        return this.setAttribute('space', val)
+    }
 
-  get limit() {
-    return this.getAttribute('limit') || '5';
-  }
+    get limit() {
+        return this.getAttribute('limit') || '5'
+    }
 
-  set limit(val) {
-    return this.setAttribute('limit', val);
-  }
+    set limit(val) {
+        return this.setAttribute('limit', val)
+    }
 
-  static get observedAttributes() {
-    return ['threshold', 'space', 'limit'];
-  }
+    static get observedAttributes() {
+        return ['threshold', 'space', 'limit']
+    }
 
-  connectedCallback() {
-    this.render();
-  }
+    connectedCallback() {
+        this.render()
+    }
 
-  attributeChangedCallback() {
-    this.render();
-  }
+    attributeChangedCallback() {
+        this.render()
+    }
 }
 
 if ('customElements' in window) {
-  
-  customElements.define('switcher-l', Switcher);
+    customElements.define('switcher-l', Switcher)
 }
