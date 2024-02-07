515911f2f67b59b990a10ffd8ac2c88b792b670a
mike connor <mike@mikeconnor.tech>
2024-02-06 18:31:38 -0600

    tuesday

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,10 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    // "eslint:recommended",
-    // "plugin:@typescript-eslint/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:astro/recommended',
+    'prettier',
   ],
   overrides: [
     {
