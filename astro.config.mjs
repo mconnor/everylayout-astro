dbb6fbdc96724b2d89942d39096f37769b26b4ed
mike connor <mike@mikeconnor.tech>
2024-02-07 16:19:17 -0600

    sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-
 import sitemap from '@astrojs/sitemap'
+
+import sentry from '@sentry/astro'
+import spotlightjs from '@spotlightjs/astro'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +11,18 @@ export default defineConfig({
   image: {
     domains: ['astro.build', 'picsum.photos'],
   },
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    sentry({
+      dsn: 'https://f1c193eae695e0d5f652cb876dc1aa08@o4506328206802944.ingest.sentry.io/4506707699171328',
+      sourceMapsUploadOptions: {
+        project: 'sentry-astro',
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    spotlightjs(),
+  ],
   vite: {
     ssr: {
       noExternal: ['open-props'],
