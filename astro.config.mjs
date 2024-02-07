e3e03772c5a8a60e014f75c6d5151360c540b407
mike connor <mike@mikeconnor.tech>
2024-02-07 16:22:14 -0600

    process

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap'
 
 import sentry from '@sentry/astro'
 import spotlightjs from '@spotlightjs/astro'
+import process from 'process'
 
 // https://astro.build/config
 export default defineConfig({
