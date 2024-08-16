d57f12be46df2dfa5e2201b74b02d3a3d2eb964e
mike connor <mike@mikeconnor.tech>
2024-08-16 10:52:58 -0500

    updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,31 @@
-import astro from 'eslint-plugin-astro';
+// @ts-check
 import astroParser from 'astro-eslint-parser';
-import markdown from 'eslint-plugin-markdown';
-import regexp from 'eslint-plugin-regexp';
-// import wc from 'eslint-plugin-wc';
-// import lit from 'eslint-plugin-lit';
-// import jsxA11y from 'eslint-plugin-jsx-a11y';
-
-import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import astro from 'eslint-plugin-astro';
 
-import react from 'eslint-plugin-react';
+import markdown from 'eslint-plugin-markdown';
+import regexp from 'eslint-plugin-regexp';
+import wc from 'eslint-plugin-wc';
+import lit from 'eslint-plugin-lit';
 
-export default tseslint.config(
+const config = tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   ...astro.configs.recommended,
-  // jsxA11y.flatConfigs.recommended,
-
   regexp.configs['flat/recommended'],
-  // wc.configs['flat/recommended'],
-  // lit.configs['flat/recommended'],
-
+  wc.configs['flat/recommended'],
+  lit.configs['flat/recommended'],
   {
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      // ecmaVersion: 'latest',
+      // sourceType: 'module',
+      parser: tseslint.parser,
       parserOptions: {
-        project: './tsconfig.eslint.json',
-
-        // For example, if you use a specific tsconfig.eslint.json for linting, you'd specify:
-        tsconfigRootDir: import.meta.dirname,
-
-        ecmaFeatures: {
-          jsx: true,
-        },
+        project: true,
       },
       globals: {
         ...globals.browser,
@@ -45,140 +34,87 @@ export default tseslint.config(
     },
   },
   {
-    rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'error',
-      '@typescript-eslint/no-unsafe-call': 'error',
-      '@typescript-eslint/no-unsafe-member-access': 'error',
-
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
+    files: ['**/*.astro'],
+    extends: [tseslint.configs.disableTypeChecked],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        project: true,
+        ecmaFeatures: {
+          jsx: true,
         },
-      ],
+        extraFileExtensions: ['.astro'],
+      },
     },
-    // 'lit/no-invalid-html': 'warn',
   },
-
-  //   rules: {
-  //     '@typescript-eslint/no-unsafe-assignment': 'warn',
-  //     '@typescript-eslint/no-unsafe-call': 'warn',
-  //     '@typescript-eslint/no-unsafe-member-access': 'warn',
-  //   },
-
   {
-    files: ['**/*.js', '**/*.mjs'],
-    ...tseslint.configs.disableTypeChecked,
+    files: ['**/*js'],
+    extends: [tseslint.configs.disableTypeChecked],
   },
-
   {
-    files: ['src/astro-custom-layout-components/**/*.js'],
-    ...tseslint.configs.disableTypeChecked,
+    files: [
+      'src/astro-custom-layout-components/**/*js',
+      'src/astro-web-component/**/*js',
+    ],
+    extends: [tseslint.configs.disableTypeChecked],
     rules: {
+      'no-unused-expressions': 'off',
       'wc/no-constructor-attributes': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+    },
+  },
+  {
+    files: ['src/schemas/**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
     },
   },
 
   {
     plugins: {
       markdown,
-      // 'jsx-a11y': jsxA11y,
-      react,
-      // 'react-hooks': fixupPluginRules(reactHooks),
     },
-  },
-  {
-    rules: {
-      // ...
-      // ...reactHooks.configs.recommended.rules,
-    },
-  },
-  {
-    files: ['**/*.astro'],
-    languageOptions: {
-      parser: astroParser,
-      parserOptions: {
-        parser: tseslint.parser,
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unsafe-assignment': 'warn',
-      '@typescript-eslint/no-unsafe-call': 'warn',
-      '@typescript-eslint/no-unsafe-member-access': 'warn',
-    },
-  },
-
-  {
-    files: ['**/*.tsx', '**/*.jsx'],
-    rules: {
-      // 'jsx-a11y/alt-text': [
-      //   2,
-      //   {
-      //     elements: ['img', 'object', 'area', 'input[type="image"]'],
-      //     img: ['Image'],
-      //     object: ['Object'],
-      //     area: ['Area'],
-      //     'input[type="image"]': ['InputImage'],
-      //   },
-      // ],
-      // 'jsx-a11y/no-autofocus': [
-      //   2,
-      //   {
-      //     ignoreNonDOM: true,
-      //   },
-      // ],
-    },
-  },
-
-  {
-    files: ['**/*.md'],
-    processor: 'markdown/markdown',
   },
   {
     files: ['**/*.md/*.js'],
-    ...tseslint.configs.disableTypeChecked,
-    // ...
+    extends: [tseslint.configs.disableTypeChecked],
     rules: {
       'no-console': 'off',
       'import/no-unresolved': 'off',
     },
   },
-
-  // {
-  //   files: ['tests/**'],
-  //   languageOptions: {
-  //     globals: {
-  //       ...globals.mocha,
-  //     },
-  //   },
-  // },
   {
     linterOptions: {
       reportUnusedDisableDirectives: 'warn',
     },
   },
+);
+
+export default [
   {
     ignores: [
-      '**/_*.*',
+      'dist',
+      '.astro',
+      '*.cjs',
+      '*rss.xml.js',
+      'src/env.d.ts',
+      'src/components/_Hamburger.astro',
+      'cache-directory/',
+      '*.d.ts',
       '**/temp.js',
       '*lock.yaml',
-      '.astro/',
-      'dist/',
-      'my-custom-cache-directory',
-      'src/env.d.ts',
       '.vercel/',
-      'src/pages/demo/modal-demo.astro',
-      'src/web-component/astro-modal/index.astro',
-      '*.config.*',
-      'src/components/ComponentLibrary.astro',
+      ' test/',
     ],
   },
+  ...config,
   eslintConfigPrettier,
-);
+];
