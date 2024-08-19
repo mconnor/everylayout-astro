02abc40c8afb6c87d7a272ac1f6440135a9d6202
mike connor <mike@mikeconnor.tech>
2024-08-18 21:54:54 -0500

    vitest.config

--- vitest.config.js
+++ vitest.config.js
@@ -1,17 +1,24 @@
 /// <reference types="vitest" />
-import { getViteConfig } from 'astro/config.js';
+import { getViteConfig } from 'astro/config';
+// import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default getViteConfig({
+  ssr: {
+    noExternal: ['date-fns', 'open-props'],
+  },
+  test: {
+    // Vitest configuration options
+  },
   build: {
     minify: 'esbuild',
     sourcemap: true, // Source map generation must be turned on
   },
-  ssr: {
-    noExternal: ['date-fns', 'open-props', '@shoelace-style/shoelace'],
-  },
-  test: {
-    /* for example, use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
-    exclude: [],
-  },
+  // plugins: [
+  //   // Put the Sentry vite plugin after all other plugins
+  //   sentryVitePlugin({
+  //     authToken: import.meta.env.SENTRY_AUTH_TOKEN,
+  //     org: 'mike-connor',
+  //     project: 'astro-openprops-blog-w-tags',
+  //   }),
+  // ],
 });
